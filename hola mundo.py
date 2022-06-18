bbf010f7eeba927d6762a043275521af95168d74
Maldo86 <107292124+Maldo86@users.noreply.github.com>
2022-06-18 19:51:38 -0400

    Add files via upload

---
+++ hola mundo.py
@@ -0,0 +1,13 @@
+from calendar import c
+
+
+print("escribe un numero")
+x=int(input(""))
+print("escribe otro numero")
+j=int(input(""))
+print("escribe otro numero")
+k=int(input(""))
+print("escribe otro numero")
+c=int(input(""))
+res=x*(2*0)+c*(2*1)+j*(2*2)+k*(2*3)
+print(res)
